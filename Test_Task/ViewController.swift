e17cf03e5919ccfe7f2722dd4a763684cd4c2c8b
farasat <farasatullahk@gmail.com>
2024-01-04 16:27:41 +0500

    Initial Commit

---
+++ Test_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_Task
+//
+//  Created by Farasat_Niazi on 04/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
